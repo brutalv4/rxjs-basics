b8fcfabbd25963798d4e13cdf19b383bc3def709
Vladimir Bredikhin <brutal.v4@gmail.com>
2022-10-09 15:16:59 +0300

    Add range example

---
+++ src/01_creation-operators/02_range.mjs
@@ -0,0 +1,11 @@
+import { range } from 'rxjs';
+
+const observer = {
+    next: val => console.log('next', val),
+    error: err => console.log('error', err),
+    complete: () => console.log('complete!'),
+};
+
+const source$ = range(1, 5);
+
+source$.subscribe(observer);
