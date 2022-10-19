2d529a4d0cfe38993e5a52ac5d3bb9d70c04a595
Vladimir Bredikhin <brutal.v4@gmail.com>
2022-10-19 12:52:02 +0300

    Add abort mission with takeUntil button clicked

--- src/labs/02_create-a-countdown-timer/index.js
+++ src/labs/02_create-a-countdown-timer/index.js
@@ -1,25 +1,32 @@
-(function ({ interval, operators: { scan, mapTo, takeWhile, tap } }) {
+(function ({
+    interval,
+    fromEvent,
+    operators: { scan, mapTo, takeWhile, tap, takeUntil },
+}) {
     // elements
     const countdownEl = document.querySelector('#countdown');
     const messageEl = document.querySelector('#message');
+    const abortBtn = document.querySelector('#abort');
 
     // helpers
-    function updateCounter(count) {
+    function render(count) {
         countdownEl.innerHTML = count;
-    }
 
-    function updateMessage() {
-        messageEl.innerHTML = 'Liftoff!';
+        if (count === 0) {
+            messageEl.innerHTML = 'Liftoff!';
+        }
     }
 
     // streams
+    const abortClick$ = fromEvent(abortBtn, 'click');
     const counter$ = interval(1000);
     counter$
         .pipe(
             mapTo(1),
             scan((accumulator, one) => accumulator - one, 10),
             tap(console.log),
-            takeWhile(value => value > 0, true)
+            takeWhile(value => value > 0, true),
+            takeUntil(abortClick$)
         )
-        .subscribe({ next: updateCounter, complete: updateMessage });
+        .subscribe(render);
 })(rxjs);
