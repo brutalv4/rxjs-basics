ef17befcfc22d5597d770c462804f4424d37577e
Vladimir Bredikhin <brutal.v4@gmail.com>
2022-10-21 13:07:08 +0300

    Update lab 2 with throttleTIme for scroll events

--- src/labs/01_scroll-progress-bar-with-fromEvent/index.js
+++ src/labs/01_scroll-progress-bar-with-fromEvent/index.js
@@ -1,11 +1,16 @@
-(function ({ fromEvent, operators: { map, tap } }) {
+(function ({
+    fromEvent,
+    asyncScheduler,
+    operators: { map, tap, throttleTime },
+}) {
+    const THROTTLE_TIME_IN_MS = 30;
+
     // elements
     const progressBar = document.querySelector('.progress-bar');
 
     // helpers
     function calculateScrollPercent({ target: { scrollingElement } }) {
         const { scrollTop, scrollHeight, clientHeight } = scrollingElement;
-
         return (scrollTop / (scrollHeight - clientHeight)) * 100;
     }
 
@@ -15,7 +20,20 @@
 
     // streams
     const scroll$ = fromEvent(document, 'scroll');
-    const progress$ = scroll$.pipe(map(calculateScrollPercent));
+    const progress$ = scroll$.pipe(
+        /*
+         * For extremely active streams like scroll events,
+         * throttleTime can be used to limit the number of emitted
+         * values. In this case, we'll just update our scroll bar every
+         * 30ms of scrolling.
+         */
+        throttleTime(THROTTLE_TIME_IN_MS, asyncScheduler, {
+            leading: false,
+            trailing: true,
+        }),
+        map(calculateScrollPercent),
+        tap(console.log)
+    );
 
     progress$.subscribe(updateProgressBar);
 })(rxjs);
